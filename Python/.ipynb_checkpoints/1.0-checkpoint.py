8467635d8c9a677ab920f1a00f4b53c7a10711a8
EliesIch <pkk70amq@s.okayama-u.ac.jp>
2020-06-23 23:01:18 +0900

    Version1.0

---
+++ Python/.ipynb_checkpoints/1.0-checkpoint.py
@@ -0,0 +1,5 @@
+import numpy as np 
+A = [[2,1,2],[3,1,0],[1,1,-1]];
+b = np.transpose([-3,5,2])
+X = np.linalg.solve(A,b)
+print("solution:\n",X)
---
+++ Python/1.0.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+a = np.array([[1, 2, 4], [3, 6, 9]])
+
+print(a)
