902ce56a8515ee6e01b12e7b0e2c9f351aca955b
EliesIch <pkk70amq@s.okayama-u.ac.jp>
2020-07-01 15:38:24 +0900

    test

--- Test.C
+++ Test.C
@@ -1,7 +1,24 @@
-#include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+void quicksort(int A[],int l,int r){
+   int m=l;
+   for(int i=l;i<r;i++){
+      if(A[i]<A[r]){
+         swap(A[i],A[m]);
+         ++m;
+      }
+   }
+   swap(A[m],A[r]);
+   if(m>l+1)quicksort(A,l,m-1);
+   if(m<r-1)quicksort(A,m+1,r);
+}
+
 int main(){
-   char name[100];
-   printf("What is your name?\n");
-   scanf("%s",name);
-   printf("Hello,%s,nice to meet you!\n",name);
+   int A[10];
+   for(int i=0;i<10;i++) A[i]=10-i;
+   for(int i=0;i<10;i++) cout<<A[i]<<' ';
+   cout<<endl;
+   quicksort(A,0,10-1);
+   for(int i=0;i<10;i++) cout<<A[i]<<' ';
 }
